71287944d9d71b4af3d4185e602282a824e79e63
walid <walid@walid.com>
2023-04-04 13:29:51 +0200

    class refactor

--- app/src/main/java/com/example/halalplaces/data/model/PlaceData.kt
+++ app/src/main/java/com/example/halalplaces/data/model/PlaceData.kt
@@ -11,7 +11,9 @@ open class PlaceData(
     var name: String,
     var latitude: Double,
     var longitude: Double,
-    var ownerId: String
+    var ownerId: String,
+    var description: String? = null,
+    var image: ByteArray? = null
 ): RealmObject {
     constructor(): this(name = "", longitude = 0.0, latitude = 0.0, ownerId = "")
 }
