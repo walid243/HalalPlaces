2b4a889602e7309f2fb6f857db0972df4a2d55f5
walid <walid@walid.com>
2023-04-04 13:28:54 +0200

    add ClickInterface

--- app/src/main/java/com/example/halalplaces/ui/map/MarkerRecycler.kt
+++ app/src/main/java/com/example/halalplaces/ui/map/MarkerRecycler.kt
@@ -4,21 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
-import com.example.halalplaces.R
 import com.example.halalplaces.data.adapter.MarkerAdapter
+import com.example.halalplaces.data.interfaces.ClickInterface
 import com.example.halalplaces.data.map.MapViewModel
+import com.example.halalplaces.data.model.Place
 import com.example.halalplaces.databinding.FragmentMarkerRecyclerBinding
 
 
-class MarkerRecycler : Fragment() {
+class MarkerRecycler : Fragment(), ClickInterface {
     private lateinit var markerAdapter: MarkerAdapter
     private lateinit var layoutManager: LayoutManager
     private lateinit var binding: FragmentMarkerRecyclerBinding
-    private val mapViewModel : MapViewModel by activityViewModels()
+    private val mapViewModel: MapViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +38,7 @@ class MarkerRecycler : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        markerAdapter = MarkerAdapter(mapViewModel.getAllMarkers())
+        markerAdapter = MarkerAdapter(mapViewModel.getAllMarkers(), this)
         layoutManager = LinearLayoutManager(context)
 
         binding.recyclerView.apply {
@@ -43,5 +46,11 @@ class MarkerRecycler : Fragment() {
             layoutManager = this@MarkerRecycler.layoutManager
             adapter = markerAdapter
         }
+    }
+
+    override fun onClick(place: Place) {
+        parentFragmentManager.setFragmentResult("place", bundleOf("place" to place))
+        val action = MarkerRecyclerDirections.actionMarkerRecyclerFragmentToDetailFragment()
+            findNavController().navigate(action)
     }
 }
